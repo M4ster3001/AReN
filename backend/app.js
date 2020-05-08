88d0bceb620295fe25a92e0060ee11167d47eb22
Aldo Junior <aldocfjunior@gmail.com>
2020-05-08 15:55:14 -0300

    criando local tmp

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ import bodyParse from 'body-parser'
 import cors from 'cors'
 import morgan from 'morgan'
 import path from 'path'
+import fs from 'fs'
 
 require( 'dotenv' ).config({ path: 'variables.env' });
 
@@ -12,6 +13,11 @@ app.use( cors() )
 app.use( bodyParse.json() ) 
 app.use( bodyParse.urlencoded({ urlencoded: true, extended: true }) ) 
 app.use( morgan('dev') )
+
+if( !fs.existsSync( './tmp/uploads' ) ) {
+    fs.mkdirSync( './tmp/uploads' )
+}
+
 app.use( '/files', express.static( path.resolve( __dirname, ".", "tmp", "uploads" ) ) )
 
 app.use( '/', router )
